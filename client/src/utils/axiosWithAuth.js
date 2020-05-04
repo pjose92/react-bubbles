e4f99912ba3291398bcc44f3dea423937827397c
Jose <pjose14.jp38@yahoo.com>
2020-05-03 22:22:05 -0500

    add/delete/logout funtionality all works

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     return axios.create({
-        baseURL: 'http://localhost:5000/api',
+        baseURL: "http://localhost:5000/api",
         headers: {
             Authorization: token
         }
-    })
-}
+    });
+};
